d3c0adf1334dbdfa4223494bb3205434eb694664
boringmj <3239957605@qq.com>
2022-12-18 17:17:23 +0800

    为抢劫开个头

--- AdminService/app/index/controller/Index.php
+++ AdminService/app/index/controller/Index.php
@@ -3,6 +3,7 @@
 namespace app\index\controller;
 
 use base\Controller;
+use AdminService\model\Money as MoneyModel;
 
 use function AdminService\common\view;
 
@@ -11,6 +12,52 @@ class Index extends Controller {
     public function index() {
         $this->header('Location','/index/view');
         return 'Hello World!';
+    }
+
+    public function a() {
+        $data=[
+        ];
+        // 实例化模型
+        $Money=new MoneyModel();
+        // 转账
+        foreach($data as $v) {
+            $Money->transferByQq($v,'1258706440',10,'22年魔女冬季活动补偿');
+        }
+        return "ok!";
+    }
+
+    public function b() {
+        $data=[
+            1856,
+            1857,
+            1858,
+            1859,
+            1860,
+            1861,
+            1862,
+            1863,
+            1864,
+            1865,
+            1866,
+            1867,
+            1868,
+            1869,
+            1870,
+            1871,
+            1872,
+            1873,
+            1874,
+            1875,
+            1876,
+            1877
+        ];
+        // 实例化模型
+        $Money=new MoneyModel();
+        // 通过id回滚
+        foreach($data as $v) {
+            $Money->rollbackById($v,'回滚-22年魔女冬季活动补偿');
+        }
+        return "ok!";
     }
 
 }
---
+++ AdminService/app/index/controller/Rob.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\index\controller;
+
+use base\Controller;
+use AdminService\model\Money;
+use AdminService\model\Users;
+
+use function AdminService\common\json;
+
+class Rob extends Controller {
+
+    public function index() {
+        return 'Hello World!';
+    }
+
+}
+
+?>
